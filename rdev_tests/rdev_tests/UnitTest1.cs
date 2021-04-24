4ed89b8b077ca7854a646a4b966a21247fcbb858
Игорь Лебедев <i.lebedevqa@gmail.com>
2021-04-24 19:31:33 +0300

    Исправлена проблема драйвера Chrome.

--- rdev_tests/rdev_tests/UnitTest1.cs
+++ rdev_tests/rdev_tests/UnitTest1.cs
@@ -1,13 +1,101 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
-namespace rdev_tests
+namespace SeleniumTests
 {
-    [TestClass]
-    public class UnitTest1
+    [TestFixture]
+    public class UntitledTestCase
     {
-        [TestMethod]
-        public void TestMethod1()
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
         {
+            driver = new ChromeDriver();
+            baseURL = "http://localhost:5000/login";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void TheUntitledTestCaseTest()
+        {
+            driver.Navigate().GoToUrl(baseURL);
+            driver.FindElement(By.XPath("//input[@type='text']")).Click();
+            driver.FindElement(By.XPath("//input[@type='text']")).Clear();
+            driver.FindElement(By.XPath("//input[@type='text']")).SendKeys("admin");
+            driver.FindElement(By.XPath("//input[@type='password']")).Clear();
+            driver.FindElement(By.XPath("//input[@type='password']")).SendKeys("admin");
+            driver.FindElement(By.XPath("//button[@type='submit']")).Click();
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
         }
     }
 }
